efff5dcaf4bf270b5a5d7fdf0600f445e4684d29
reyunj <junrey1296@gmail.com>
2025-02-13 21:24:27 +0800

    Adjust padding header

--- app/components/Header.js
+++ app/components/Header.js
@@ -24,12 +24,14 @@ const Header = () => {
         </div>
         {showHomeButton && (
           <Link href="/" className="flex items-center">
+            <div className='pr-5'>
             <button className="bg-blue-400 hover:bg-blue-500 text-white px-4 py-1.5 rounded-lg transition-colors duration-200 flex items-center gap-2">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
               </svg>
               Return to Home
             </button>
+            </div>
           </Link>
         )}
       </div>
